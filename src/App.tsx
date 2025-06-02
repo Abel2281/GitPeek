81f15fc008ff6323b23df363fcb078e02a51c5b1
Abel S Mathew <abelsmathew228@gmail.com>
2025-06-02 19:44:56 +0530

    wrapped all components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
 import './App.css'
+import Navbar from './components/Navbar'
+import Inputbar from './components/Inputbar'
+import { CssBaseline } from '@mui/material';
+import UserCard from './components/UserCard';
 
 function App() {
 
   return (
     <>
-      
+      <CssBaseline />
+      <Navbar />
+      <Inputbar />
+      <UserCard />
     </>
   )
 }
