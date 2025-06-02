a2341eb4d486a8e002958ff5a31777be92f59636
Abel S Mathew <abelsmathew228@gmail.com>
2025-06-02 19:42:36 +0530

    wrapped context provider and custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react'
+import theme from './theme.tsx'
+
+import { GitHubProvider } from './context/Githubusercontext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider theme={theme}>
+      <StrictMode>
+        <GitHubProvider>
+           <App />
+        </GitHubProvider>
+      </StrictMode>,
+  </ThemeProvider>
+  
 )
