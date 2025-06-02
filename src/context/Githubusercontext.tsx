1deaad06e4aea5e783fee967885460bea3b12959
Abel S Mathew <abelsmathew228@gmail.com>
2025-06-02 19:40:38 +0530

    integrate Context API for global GitHub user state management

---
+++ src/context/Githubusercontext.tsx
@@ -0,0 +1,36 @@
+import {createContext, useContext, useState} from 'react';
+
+export interface GithubUser {
+    name: string;
+    followers: number;
+    following: number;
+    avatar_url: string;
+    public_repos: number;
+    bio: string;
+}
+
+interface GithubContextType {
+    userdata : GithubUser | null;
+    setUserdata : React.Dispatch<React.SetStateAction<GithubUser | null>>;
+}
+
+const GitHubContext = createContext<GithubContextType | undefined>(undefined);
+
+export  const GitHubProvider = ({ children }: { children: React.ReactNode }) => {
+    const [userdata,setUserdata] = useState<GithubUser | null>(null);
+
+    return(
+        <GitHubContext.Provider value={{userdata,setUserdata}}>
+            {children}
+        </GitHubContext.Provider>
+    )
+    
+}
+
+export const useGitHub = (): GithubContextType => {
+  const context = useContext(GitHubContext);
+  if (!context) {
+    throw new Error('useGitHub must be used within a GitHubProvider');
+  }
+  return context;
+}
